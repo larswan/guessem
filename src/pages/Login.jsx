2e6154e1f5c4afea4d226d846b59f43b7fb0470e
Larson Collier <collierlarson@gmail.com>
2023-02-06 17:47:07 -0500

    copied in google login stuff

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,61 +1,53 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from 'react'
+import { gapi } from 'gapi-script';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
-function Login() {
+
+const Login = ({}) => {
     const navigate = useNavigate()
-
-    const [users, setUsers] = useState([])
-    const [currentUser, setCurrentUser] = useState([])
-    const [formData, setFormData] = useState({
-        email: '',
-        password: ''
-    })
-
-
+    const clientId = import.meta.env.GOOGLE__CLIENT_ID
+    const [userObj, setUserObj] = useState()
+    
     useEffect(() => {
-        let request = async () => {
-            let req = await fetch(`http://localhost:3000/users`)
-            let res = await req.json()
-            setUsers(res)
-        }
-        request()
-    }, [])
+        const initClient = () => {
+            gapi.client.init({
+                clientId: clientId,
+                scope: ''
+            });
+        };
+        gapi.load('client:auth2', initClient);
+    });
 
+    const logOut = () => {
+        setProfile(null);
+        navigate("/")
+    };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        users.some(user => {
-            if (user.email === formData.email && user.password === formData.password) {
-                setCurrentUser(user)
-                console.log("welcome back!")
-                navigate('/profile')
-            } else {
-                console.log('user not found', currentUser)
-            }
-        })
-            ;
+    const onSuccess = async (res) => {
+        console.log('success:', res);
+        setUserObj(res.profileObj);
+        console.log(res)
+        navigate("/")
+        // console.log(res.profileObj.imageUrl)
     }
-
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value })
-    }
-
+    const onFailure = (err) => {
+        console.log('failed:', err);
+    };
 
     return (
-        <div className="grandparent-form-div">
-            <h2 className="gallery-header">ENTER YOUR GALLERY</h2><br />
-            <div className="parent-submit-div">
-                <div className="" onSubmit={(e) => { handleSubmit(e) }}>
-                    <h2 className="login-header">LOG IN</h2><br />
-                    <form className="form">
-                        <input className="input-field" type="email" value={formData.email} name='email' onChange={e => handleChange(e)} placeholder="EMAIL" /><br />
-                        <input className="input-field" type="password" value={formData.password} name='password' onChange={e => handleChange(e)} placeholder="PASSWORD" /><br />
-                        <div className="submit-button-div">
-                            <input className="submit-button" type="submit" />
-                        </div>
-                    </form>
-                </div>
+        <div className="footerContainer">
+            <div className="login">
+                {profile ? (<GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />) : (
+                    <GoogleLogin
+                        clientId={clientId}
+                        buttonText="Sign in with Google"
+                        onSuccess={onSuccess}
+                        onFailure={onFailure}
+                        cookiePolicy={'single_host_origin'}
+                        isSignedIn={true} />
+                )}
             </div>
         </div>
     )
