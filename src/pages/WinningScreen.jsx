eb4a199399724ef54d864a0350e733bcc0e3595c
Larson Collier <collierlarson@gmail.com>
2023-02-14 05:58:28 -0500

    stylized winningScreen

--- src/pages/WinningScreen.jsx
+++ src/pages/WinningScreen.jsx
@@ -1,16 +1,23 @@
 import BackButton from "../components/BackButton"
+import Header from "../components/Header"
 
 const WinningScreen = ({ opponent, setPhase, gameData, user, player}) => {
-    console.log(gameData)
+    console.log(gameData, user)
+    const youWon = (gameData.game.winningUser == user.id)
+
     return (
         <div>
-            <BackButton />
-            <h1 className="Subheader">
-            Winning screen:  {gameData.game.winningUser} guessed {gameData.game.winningCard.name} off this question: {gameData.game.winningQuestion} {gameData.game.winningAnswer}
-            </h1>
-            <img src={gameData.winningCard.image} />
+            <Header />
+            {
+                youWon ? <h1 className="victoryText">YOU WON!</h1> : <h1 className="victoryText">you lost...</h1>
+            }
+            <img className="cardBig" src={gameData.winningCard.image} />            
+            <div className="flex">
+                <h1 className="QAheader" >{user.name} knew it was {gameData.winningCard.name} from this question: </h1>
+            </div>
+            <h1 className="QAtext" >{gameData.game.winningQuestion}</h1>
+            <h1 className="Subheader" >{gameData.game.winningAnswer}</h1>
         </div>
-        
     )
 }
 export default WinningScreen
