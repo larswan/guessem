97e54c87c99149eed3e42c7c93dfd19c8cccdf0b
Larson Collier <collierlarson@gmail.com>
2023-02-16 11:25:39 -0500

    stlying guessmode button

--- src/components/GuessModeButton.jsx
+++ src/components/GuessModeButton.jsx
@@ -7,7 +7,7 @@ const GuessModeButton = ({guessMode, setGuessMode, setQuestion}) => {
             console.log(guessMode)
         }
         else{
-            setQuestion("Click a card!")
+            setQuestion("Press again to turn off guess mode...")
             setGuessMode(!guessMode)
             console.log(guessMode)
         }
@@ -15,7 +15,7 @@ const GuessModeButton = ({guessMode, setGuessMode, setQuestion}) => {
 
     return(
         <div className={guessMode ? "guessButtonOn" : "guessButtonOff" } >
-            <button onClick={() => { handleClick() }} className="font-black bg-blackish py-1 px-2 text-lightYellow my-4 rounded-sm" >GUESS A CARD</button>
+            <button onClick={() => { handleClick() }} className="font-black bg-blackish w-full py-1 px-2 text-lightYellow my-4 rounded-sm" >{guessMode ? "CHOOSE A CARD!" :"GUESS A CARD"}</button>
             {/* <div onClick={()=>{handleClick()}} className="font-black bg-lightBlackish p-2 ml-2 my-2 rounded-lg w-1/4">
                 <img src="backend/app/assets/images/designAssetts/logo trans grey color.png"  />
             </div> */}
